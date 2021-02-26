7e3d0f45f7cc8dafa69a5496bf9e3b7cff3a0a58
Sam Basu <samidip@live.com>
2021-02-25 21:29:16 -0500

    Initial commit

---
+++ Hubs/DashboardHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Hubs
+{
+    public class DashboardHub : Hub
+    {
+        public async Task SendDashboardUpdate(string dashupdate)
+        {
+            await Clients.All.SendAsync("ReceiveDashUpdate", dashupdate);
+        }
+
+        public async Task SendObjectUpdate(ChartData[] chartUpdate)
+        {
+            await Clients.All.SendAsync("ReceiveObjUpdate", chartUpdate);
+        }
+    }
+
+    public class ChartData
+    {
+        public string Category { get; set; }
+        public int Value { get; set; }
+    }
+}
---
+++ obj/Release/netcoreapp3.0/PubTmp/Out/wwwroot/js/dashboard.js
@@ -0,0 +1,34 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/dashboardHub").build();
+
+document.getElementById("sendDashButton").disabled = true;
+
+connection.on("ReceiveDashUpdate", function (dashupdate) {
+    var encodedMsg = "Dash Update: " + dashupdate;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("dashUpdateList").appendChild(li);
+});
+
+connection.start().then(function () {
+    document.getElementById("sendDashButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendDashButton").addEventListener("click", function (event) {
+    var gaugeupdate = document.getElementById("gaugeValue").value;    
+    connection.invoke("SendDashboardUpdate", gaugeupdate).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+document.getElementById("sendObjButton").addEventListener("click", function (event) {
+    var chartdata = [{ "category": "A", "value": 5 }, { "category": "B", "value": 10 }];
+    connection.invoke("SendObjectUpdate", chartdata).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
